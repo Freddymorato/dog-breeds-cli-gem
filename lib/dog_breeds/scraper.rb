503493d804bebd0062a27dc6b0d3b74019d80d72
Frederico  Morato <freddymorato@gmail.com>
2018-04-07 20:00:03 +0000

    Scraper class can scrape users from the index page

--- lib/dog_breeds/scraper.rb
+++ lib/dog_breeds/scraper.rb
@@ -1,11 +1,20 @@
 class DogBreeds::Scraper
 
-  def self.scrape_index(url)
-    dog_breeds = []
-    doc = Nokogiri::HTML(open(url))
-  end
+	def self.scrape_index(url) 
+		dog_breeds = []
+		doc = Nokogiri::HTML(open(url))
+		breeds = doc.css(".group-list-item .post-title")
+
+		breeds.each do |b|
+			breed = {}
+			breed[:name] = b.text
+			breed[:page_url] = b.attr("href")
+			dog_breeds << breed
+		end
+		dog_breeds
+	end
 
   def self.scrape_profile(url)
-    doc = Nokogiri::HTML(open(url)) 
+    doc = Nokogiri::HTML(open(url))
   end
 end
