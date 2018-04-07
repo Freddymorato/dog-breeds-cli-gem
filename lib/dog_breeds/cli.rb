b52b1d4d204d7a99711cc3c3ddca48ab5f1ea6f7
Frederico  Morato <freddymorato@gmail.com>
2018-04-07 19:29:36 +0000

    cli method make_dogs scrapes profiles

--- lib/dog_breeds/cli.rb
+++ lib/dog_breeds/cli.rb
@@ -1,11 +1,17 @@
 class DogBreeds::CLI
 
+	BASE_PATH = "http://dogtime.com"
+
 	def call
 		breeds = make_dogs
 		list_dogs(breeds)
 	end
 
 	def make_dogs
+		breeds_array = DogBreeds::Scraper.scrape_index(BASE_PATH + "/dog-breeds/profiles")
+		breeds_array.collect do |breed|
+				DogBreeds::Dog.new(breed[:name], breed[:page_url])
+			end
 	end
 
 	def list_dogs
