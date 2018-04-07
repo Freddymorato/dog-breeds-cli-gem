7de84aeddbf005b401b21298d7b876ce6609f1c3
Frederico  Morato <freddymorato@gmail.com>
2018-04-07 19:24:13 +0000

    set up basic method structure for cli file

--- lib/dog_breeds/cli.rb
+++ lib/dog_breeds/cli.rb
@@ -1,6 +1,25 @@
 class DogBreeds::CLI
 
 	def call
+		breeds = make_dogs
+		list_dogs(breeds)
+	end
 
+	def make_dogs
+	end
+
+	def list_dogs
+	end
+
+	def view_breed_overview(breed)
+	end
+
+	def view_more_details(breed)
+	end
+
+	def view_topic()
+	end
+
+	def goodbye
 	end
 end
