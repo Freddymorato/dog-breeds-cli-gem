ae290a0f9d1c66395acc31d91515fa5385fd4a80
Frederico  Morato <freddymorato@gmail.com>
2018-04-07 19:35:39 +0000

    Dog class initializes a dog with attributes

--- lib/dog_breeds/dog.rb
+++ lib/dog_breeds/dog.rb
@@ -1,5 +1,21 @@
 class DogBreeds::Dog
+  attr_accessor :name, :page_url
 
   @@all = []
 
+  def initialize(name, page_url)
+    @name = name
+    @page_url = page_url
+    @@all << self
+  end
+
+  def ad_details(details)
+    details.each do |k,v|
+      self.send("#{k}=", v)
+    end
+  end
+
+  def self.all
+    @@all 
+  end
 end
