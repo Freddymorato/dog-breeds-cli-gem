b8844b67574a36a12258ab03ee27679a6c4ee89f
Frederico  Morato <freddymorato@gmail.com>
2018-04-07 20:04:48 +0000

    added more attributes to Dog class

--- lib/dog_breeds/dog.rb
+++ lib/dog_breeds/dog.rb
@@ -1,21 +1,21 @@
 class DogBreeds::Dog
-  attr_accessor :name, :page_url
+	attr_accessor :name, :page_url, :intro, :highlights, :history, :personality, :size, :care, :feeding, :children_and_other_pets
 
-  @@all = []
+	@@all = []
 
-  def initialize(name, page_url)
-    @name = name
-    @page_url = page_url
-    @@all << self
-  end
+	def initialize(name, page_url)
+		@name = name
+		@page_url = page_url
+		@@all << self
+	end
 
-  def ad_details(details)
-    details.each do |k,v|
-      self.send("#{k}=", v)
-    end
-  end
+	def add_details(details) #takes hash returned from DogBreeds::Scraper.scrape_profile(url) and adds breed data to the corresponding instance of dog breed
+		details.each do |k,v|
+			self.send("#{k}=", v)
+		end
+	end
 
-  def self.all
-    @@all 
-  end
+	def self.all
+		@@all
+	end
 end
