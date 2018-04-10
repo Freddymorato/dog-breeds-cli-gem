678799f6084947d442cecb2cd1710083cebc7679
Frederico  Morato <freddymorato@gmail.com>
2018-04-10 18:00:57 +0000

    updated dog_breeds.rb

--- lib/dog_breeds.rb
+++ lib/dog_breeds.rb
@@ -3,7 +3,9 @@ require "nokogiri"
 require "open-uri"
 
 
-require_relative "./dog_breeds/version"
+require "dog_breeds/version"
+
+
 require_relative "./dog_breeds/cli"
 require_relative "./dog_breeds/scraper.rb"
 require_relative "./dog_breeds/dog.rb"
