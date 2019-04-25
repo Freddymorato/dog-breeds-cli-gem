e2e863fe8f005fc1443ed2a30a598ee8ddcd7e5b
Freddymorato <Freddymorato@gmail.com>
2019-04-25 17:49:03 -0400

    updated scraper node selectors

--- lib/dog_breeds/scraper.rb
+++ lib/dog_breeds/scraper.rb
@@ -3,8 +3,7 @@ class DogBreeds::Scraper
 	def self.scrape_index(url) #creates and returns an array of hashes which contain dog breeds and page urls
 		dog_breeds = []
 		doc = Nokogiri::HTML(open(url))
-		breeds = doc.css(".group-list-item .post-title")
-
+		breeds = doc.css(".list-item .list-item-title")
 		breeds.each do |b|
 			breed = {}
 			breed[:name] = b.text
@@ -17,27 +16,25 @@ class DogBreeds::Scraper
 	def self.scrape_profile(url) #creates and returns a hash of a dog breed with more information for the user to view
 		doc = Nokogiri::HTML(open(url))
 		attributes = {}
-
-		y = doc.css("header p")
+		y = doc.css(".breeds-single-intro p")
 		y.each do |x|
 			attributes[:intro] = x.text if x.text.include? "dog breed"
 		end
-
 		list = doc.css(".breed-data-item.js-accordion-item.item-expandable-content")
 		list.each do |i|
-			if i.css("h2").text.include? "Highlights"
+			if i.css("h3").text.include? "Highlights"
 				 attributes[:highlights] = i.css("div").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub("\"","")
-			 elsif i.css("h2").text.include? "History"
+			 elsif i.css("h3").text.include? "History"
 				 attributes[:history] = i.css("div").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub("\"","")
-			 elsif i.css("h2").text.include? "Size"
+			 elsif i.css("h3").text.include? "Size"
 				 attributes[:size] = i.css(".breed-data-item-content.js-breed-data-section").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub("\"","")
-			 elsif i.css("h2").text.include? "Personality"
+			 elsif i.css("h3").text.include? "Personality"
 				 attributes[:personality] = i.css("div").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub("\"","")
-			 elsif i.css("h2").text.include? "Care"
+			 elsif i.css("h3").text.include? "Care"
 				 attributes[:care] = i.css("div").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub("\"","")
-			 elsif i.css("h2").text.include? "Feeding"
+			 elsif i.css("h3").text.include? "Feeding"
 				 attributes[:feeding] = i.css(".breed-data-item-content.js-breed-data-section").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub("\"","")
-			 elsif i.css("h2").text.include? "Children And Other Pets"
+			 elsif i.css("h3").text.include? "Children And Other Pets"
 				 attributes[:children_and_other_pets] = i.css("div").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub("\"","")
 			 else
 				 nil
